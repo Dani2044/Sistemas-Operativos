fa555bf8ff4782bcc1b19f078daec635d45726dc
Dani2044 <danielfcm2004@gmail.com>
2024-07-21 16:39:22 -0500

    Add files via upload

---
+++ example0.c
@@ -0,0 +1,31 @@
+/****************************
+Autor: Daniel Castro
+Materia: Sistemas operativos
+Fecha: 18/07/24
+Tema: Introducción al lenguaje C y los entornos
+  -Entornos en la web (CLOUD/NUBE)
+  -Comandos principales de Linux
+  -Programa 0: Hola mundo
+****************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  // Variables
+  char nombre[] = "Daniel Castro";
+  int edad = 19;
+  char carrera[] = "Ingeniería de Sistemas";
+  int semestre = 4;
+  char universidad[] = "Universidad Javeriana";
+
+  // Imprimir la información del estudiante
+  printf("Información del Estudiante\n");
+  printf("--------------------------\n");
+  printf("Nombre: %s\n", nombre);
+  printf("Edad: %d años\n", edad);
+  printf("Carrera: %s\n", carrera);
+  printf("Semestre: %d\n", semestre);
+  printf("Universidad: %s\n", universidad);
+
+  return 0; 
+}
