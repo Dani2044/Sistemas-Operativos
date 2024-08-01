044a9ffe182532aa8600de9025a3a7bf417b8aab
Daniel Castro <danielfcm2004@gmail.com>
2024-08-01 11:27:58 -0500

    Update

---
+++ example6.c
@@ -0,0 +1,27 @@
+/*************************************************
+Autor: Daniel Castro
+Materia: Sistemas operativos
+Fecha: 1/08/24
+Tema: Sistemas operativos, parte 2
+**************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *function(char *p);
+
+int main(){
+  char *a = NULL;
+  char *b = NULL;
+  a = function("Hi, it's fun to learn");
+  b = function("Systems engineering");
+  printf("From main: %s %s\n", a, b);
+}
+
+char *function(char *p){
+  char q[strlen(p) + 1];
+  strcpy(q, p);
+  printf(" From q: the string is %s\n", q);
+  return *q;
+}
