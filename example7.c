3e03d9758bf495759eb80fefc3d43d3d981b9e97
Daniel Castro <danielfcm2004@gmail.com>
2024-08-15 10:50:50 -0500

    Update example7.c

--- example7.c
+++ example7.c
@@ -11,6 +11,8 @@ Tema: Sistemas operativos, parte 2
 int main(int argc, char *argv[]){
   int variable = 123;
   int *ptr = &variable;
+
+  // Lectura de valores y dirección de memoria
   printf("Valor de la variable: %d\n", variable);
   printf("Dirección de memoria de la variable: %p\n", &variable);
   printf("Dirección a la que apunta el puntero: %p\n", (void *)ptr);
