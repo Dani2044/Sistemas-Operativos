0c062847f7d78ae9992063a8eaa22d5f8446bdee
Daniel Castro <danielfcm2004@gmail.com>
2024-08-15 10:42:04 -0500

    Update example11.c

--- example11.c
+++ example11.c
@@ -13,15 +13,16 @@ int main(int argc, char *argv[]){
   int var = 480;
   int *varptr = &var;
   int **dobleptr = &varptr;
+
+  // Valores
   printf("Valor de la variable \t\t= %d\n", var);
   printf("Valor del puntero \t\t= %d\n", *varptr);
   printf("Valor del puntero doble \t\t= %d\n", **dobleptr);
-  
+
+  // Direcciones y valores de punteros
   printf("Dirección de la variable \t\t= %p\n", &varptr);
-  
   printf("Dirección del puntero \t\t= %p\n", &varptr);
   printf("Valor en el puntero \t\t= %p\n", varptr);
-  
   printf("Dirección del puntero doble \t\t= %p\n", &dobleptr);
   printf("Valor en el puntero doble \t\t= %p\n", dobleptr);
   
