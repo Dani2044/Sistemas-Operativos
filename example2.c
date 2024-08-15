231dc8ca34abbeaa535d584a710bd7c544560ae6
Daniel Castro <danielfcm2004@gmail.com>
2024-08-15 10:45:20 -0500

    Update example2.c

--- example2.c
+++ example2.c
@@ -11,10 +11,14 @@ Tema: Sistemas operativos, parte 2
 int main(int argc, char* argv[]){
   int x;
   int suma = 0;
+  
   do{
+    // Leer valores por consola
     scanf("%d", &x);
+    // Calcular suma
     suma += x;
   } while(x != 0);
+  // Imprimir
   printf("La suma es: %d.\n", suma);
   return 0;
 }
