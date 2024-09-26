2ecc7c542229f25dcba1d0ef3a1401d908831973
Daniel Castro <castro-df@javeriana.edu.co>
2024-09-26 11:59:20 -0500

    Create fork

---
+++ Fork.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+  int x;
+  for(x=0;x<5;x++) {
+    fork();
+    printf("The process ID (PID): %d \n",getpid());           
+  }
+  return 0;
+}
