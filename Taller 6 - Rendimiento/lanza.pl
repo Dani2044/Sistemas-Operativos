555d1ca4dc19ce56d08eb9514132b496b00f50a0
Daniel Castro <danielfcm2004@gmail.com>
2024-11-05 16:19:52 -0500

    Update lanza.pl

--- Taller 6 - Rendimiento/lanza.pl
+++ Taller 6 - Rendimiento/lanza.pl
@@ -7,74 +7,66 @@
 #Descripción:
 #    Este programa realiza la multiplicación de matrices cuadradas N×N empleando el modelo
 #    de hilos POSIX (Pthreads) y evalúa el tiempo de ejecución del algoritmo clásico de 
-#    multiplicación de matrices distribuyendo el trabajo en múltiples hilos
+#    multiplicación de matrices distribuyendo el trabajo en múltiples hilos.
 #************************************************************************************************/
 
-use strict;
-use warnings;
-
-# Ruta del directorio de trabajo actual
-my $Path = `pwd`;
+# Obtiene la ruta del directorio de trabajo actual y elimina el salto de línea
+$Path = `pwd`;
 chomp($Path);
 
 # Nombres de los ejecutables que se van a ejecutar
-my @Nombres_Ejecutables = ("mm_clasico", "mm_transpuesta");
+@Nombres_Ejecutables = ("mm_clasico", "mm_transpuesta");
 
 # Tamaños de las matrices a evaluar
-my @Size_Matriz = (100, 200, 300, 500, 1000);
+@Size_Matriz = (100, 200, 500, 1000, 2000);
 
 # Número de hilos a utilizar en la ejecución
-my @Num_Hilos = (1, 2, 4, 8, 16);
+@Num_Hilos = (1, 2, 4, 8, 16);
 
 # Número de repeticiones para cada combinación de tamaño de matriz y número de hilos
-my $Repeticiones = 30;
+$Repeticiones = 30;
 
-# Nombre del archivo de salida
-my $output_file = "$Path/resultados_promedio.dat";
+# Nombre del archivo para guardar la tabla de resultados
+my $archivo_resultados = "$Path/resultados_promedios.dat";
 
-# Abre el archivo de salida para escribir los resultados
-open(my $out_fh, '>', $output_file) or die "No se pudo abrir el archivo $output_file: $!";
+# Abrir archivo de resultados para escribir la tabla final
+open(my $fh, '>', $archivo_resultados) or die "No se puede abrir el archivo: $!";
 
-# Escribe el encabezado de la tabla en el archivo de salida
-print $out_fh "\t\tMáquina x\n";
-print $out_fh "\tmm_clasico\t\t\t\t\tmm_transpuesta\n";
-print $out_fh "\t1\t2\t4\t8\t16\t1\t2\t4\t8\t16\n";
+# Imprimir encabezado de la tabla en el archivo
+print $fh "\tMáquina x\n";
+print $fh "\tmm_clasico\t\t\t\t\t\t\t\t\t\tmm_transpuesta\n";
+print $fh "\t1\t\t2\t\t4\t\t8\t\t16\t\t1\t\t2\t\t4\t\t8\t\t16\n";
 
-# Itera sobre cada tamaño de matriz
+# Bucle que itera sobre cada tamaño de matriz
 foreach my $size (@Size_Matriz) {
-    print $out_fh "$size\t";  # Escribe el tamaño de la matriz en la primera columna
-
-    # Itera sobre cada ejecutable
+    # Línea de la tabla para el tamaño actual
+    print $fh "$size\t";
+    
+    # Bucle que itera sobre los ejecutables
     foreach my $ejecutable (@Nombres_Ejecutables) {
-        # Itera sobre el número de hilos
+        # Bucle que itera sobre el número de hilos
         foreach my $hilo (@Num_Hilos) {
-            my $total_time = 0;
+            my $suma_tiempos = 0;
 
-            # Ejecuta el programa repetidamente y acumula los tiempos
+            # Bucle para repetir la ejecución del programa
             for (my $i = 0; $i < $Repeticiones; $i++) {
-                my $output = `$Path/$ejecutable $size $hilo`;
+                # Ejecuta el programa y captura la salida del tiempo de ejecución
+                my $tiempo = `$Path/$ejecutable $size $hilo`;
                 
-                # Si el programa genera la salida en milisegundos (ejemplo), obtenla del output
-                # Verifica si el valor de tiempo está en la salida (asumimos un solo valor de tiempo en ms)
-                if ($output =~ /(\d+(\.\d+)?)/) {
-                    $total_time += $1;
-                } else {
-                    die "No se encontró un valor de tiempo en la salida de $ejecutable con tamaño $size y $hilo hilos.";
-                }
+                # Acumular el tiempo para calcular el promedio
+                $suma_tiempos += $tiempo;
             }
+            # Calcula el promedio del tiempo de ejecución
+            my $promedio = $suma_tiempos / $Repeticiones;
             
-            # Calcula el promedio de tiempo de ejecución
-            my $average_time = $total_time / $Repeticiones;
-            
-            # Escribe el promedio en el archivo
-            printf $out_fh "%.2f\t", $average_time;
+            # Escribe el promedio en la tabla
+            print $fh "$promedio\t";
         }
     }
-    
-    # Nueva línea después de cada fila de tamaño de matriz
-    print $out_fh "\n";
+    print $fh "\n"; # Nueva línea para el siguiente tamaño
 }
 
-# Cierra el archivo de salida
-close($out_fh);
-print "Resultados promedios guardados en $output_file\n";
+# Cerrar el archivo de resultados
+close($fh);
+
+print "La tabla de promedios ha sido guardada en $archivo_resultados\n";
